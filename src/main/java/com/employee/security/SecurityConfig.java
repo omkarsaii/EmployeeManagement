914de1bf82d6c218cf0cac492e42fdc5065b68ac
omkarsaii <omkarsai77@gmail.com>
2024-12-01 12:35:39 +0530

    added security

---
+++ src/main/java/com/employee/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.employee.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		return 
+		http
+		.csrf(Customizer->Customizer.disable()) //csrf().disable()
+		.authorizeHttpRequests(request ->request.anyRequest().authenticated())
+//		.formLogin(Customizer.withDefaults())  //it will enable in browser like chrome 
+		.httpBasic(Customizer.withDefaults())  // enable postman
+		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.build();
+	}
+}
