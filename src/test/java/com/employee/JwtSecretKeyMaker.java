33322b5aede3aa6f5b17c21d629aebf950e2042e
OmkarFsc <omkarbellary88@gmail.com>
2024-12-03 22:35:09 +0530

    tested both algo

--- src/test/java/com/employee/JwtSecretKeyMaker.java
+++ src/test/java/com/employee/JwtSecretKeyMaker.java
@@ -13,7 +13,7 @@ public class JwtSecretKeyMaker {
 	public void generateKey()
 	{
 	//   SecretKey key=Jwts.SIG.HS512.key().build();
-    SecretKey key=Jwts.SIG.HS256.key().build();
+      SecretKey key=Jwts.SIG.HS256.key().build();
 	  String encodedkey=DatatypeConverter.printHexBinary(key.getEncoded());
 	  System.out.printf("\nKEY =[%s]\n",encodedkey);
 	}
