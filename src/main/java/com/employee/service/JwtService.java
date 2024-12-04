60c4bd6f94f0bf07ab0cf6e661e02a86c42c6e48
OmkarFsc <omkarbellary88@gmail.com>
2024-12-04 14:15:40 +0530

    added JWT service and Filter

--- src/main/java/com/employee/service/JwtService.java
+++ src/main/java/com/employee/service/JwtService.java
@@ -5,10 +5,15 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -53,6 +58,42 @@ public class JwtService {
 		byte[] decodedkey=Base64.getDecoder().decode(SECRET);
 		return Keys.hmacShaKeyFor(decodedkey);
 		
+	}
+
+	public String extractUsername(String token) {
+		
+		return extractClaim(token,Claims::getSubject);
+	}
+
+	private<T> T extractClaim(String token, Function<Claims,T> claimResolver) {
+		final Claims claims=extractAllClaims(token);
+		return claimResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		
+		return Jwts.parser()
+				.verifyWith(getkey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+				
+	}
+
+	public boolean validateToken(String token, UserDetails userDetails) {
+		final String userName=extractUsername(token);
+		
+		return (userName.equals(userDetails.getUsername())&& !isTokenExpired(token));
+	}
+
+	private boolean isTokenExpired(String token) {
+		
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		
+		return extractClaim(token, Claims::getExpiration);
 	}
 
 }
